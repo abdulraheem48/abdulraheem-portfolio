d149a76565bb48a4cfd383cba84342ecec45e1bb
raheem_mac <abdulraheeem687@gmail.com>
2025-10-31 19:49:44 +0530

    Added Apple Big Data Engineer section and glow effects for icons & Download CV button

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const text = ["Data Engineer", "Cloud Engineer", "Big Data Engineer"];
-let index = 0;
+// Typing Animation
+const roles = ["Data Engineer", "Azure Developer", "Big Data Engineer"];
+let roleIndex = 0;
 let charIndex = 0;
-const typingElement = document.querySelector(".typing");
+let currentRole = "";
+let currentChar = "";
 
-function typeEffect() {
-  if (charIndex < text[index].length) {
-    typingElement.textContent += text[index].charAt(charIndex);
-    charIndex++;
-    setTimeout(typeEffect, 100);
+(function type() {
+  if (roleIndex === roles.length) roleIndex = 0;
+  currentRole = roles[roleIndex];
+  currentChar = currentRole.slice(0, ++charIndex);
+  document.querySelector(".typing").textContent = currentChar;
+
+  if (currentChar.length === currentRole.length) {
+    roleIndex++;
+    charIndex = 0;
+    setTimeout(type, 1200);
   } else {
-    setTimeout(eraseEffect, 1500);
+    setTimeout(type, 120);
   }
-}
-
-function eraseEffect() {
-  if (charIndex > 0) {
-    typingElement.textContent = text[index].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(eraseEffect, 50);
-  } else {
-    index = (index + 1) % text.length;
-    setTimeout(typeEffect, 200);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", typeEffect);
+})();
